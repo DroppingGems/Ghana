71defd31cb38acd024bd54598db5d1ae78452a5a
Simmons <simmons.363@osu.edu>
2020-08-11 13:11:54 -0400

    Working On Mortgage Website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
-class ApplicationController < ActionController::Base
+# class ApplicationController < ActionController::Base
+#   def index
+#   end
+# end
+
+class MortgagesController < ApplicationController
   def index
+    @mortgages = Mortgage.all
   end
 end
-
-
---
+++ app/lib/giraffe/play_ground.rb
@@ -0,0 +1,28 @@
+<!--<h1>Mortgages coming soon!</h1>-->
+<!--<p>-->
+<%#= form_with scope: :mortgages, url: mortgages_path, local: true do |form| %>
+<!--</p>-->
+<!--    <p>-->
+<%#= form.label :title %>
+      <%#= form.text_field :title %>
+                              <!--    </p>-->
+<!--    <p>-->
+      <%#= form.label :text %>
+      <%#= form.text_area :text %>
+<!--    </p>-->
+<!--    <p>-->
+<%#= form.submit %>
+<!--    </p>-->
+<!--    <p>-->
+      <%# end %>
+
+
+    <!--# Type of Property ; Address ; Asking Price-->
+
+<!--# Sqft ; Bedrooms ; Bathroom-->
+
+<!--# 20% down ; 15% down ; 10% down-->
+
+<!--# (balance * (( i / 1200 ) * ( 1 + ( i / 1200 )) ^ Length ) / (( 1 + ( i / 1200 )) ^ Length - 1 ), 2 )-->
+
+
